e1776a20cbb79c0cf186b1db4ca134593276cd13
Dawid Kufel <kufdaw@gmail.com>
2019-03-21 22:25:43 +0100

    Welcome page project basic desc added.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -19,81 +18,29 @@
                 height: 100vh;
                 margin: 0;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div style="margin: 48px;">
+            <h2>Uruchamianie kontenerów dockera</h2>
+            <code>docker-compose up -d</code>
+            <hr>
+            <h2>Bash dockerowy</h2>
+            <code>docker-compose exec php bash</code>
+            <hr>
+            <h2>Rzeczy, które robimy z basha dockera</h2>
+            <ul>
+                <li>migracje</li>
+                <li>wszystko composerowe</li>
+                <li>php artisan tinker</li>
+                <li>ogolnie artisanowe rzeczy zwiazane z baza danych (ale juz np tworzenie migracji lokalnie) - to co wymaga polaczenia z baza</li>
+            </ul>
+            <hr>
+            <h2>Rzeczy ktore robimy lokalnie</h2>
+            <ul>
+                <li>wszystko zwiazane z gitem</li>
+                <li>tworzenie nowych struktur poprzez artisana, czyli np. modeli, kontrolerow, migracji</li>
+            </ul>
         </div>
     </body>
 </html>
