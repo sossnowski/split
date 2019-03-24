fca9dddb5ee93c0a27ee7228a496633ea0e308a2
Krzysztof Brzoska <krzysztofbrzoska@MacBook-Pro-Krzysztof.local>
2019-03-24 13:17:27 +0100

    foreign keys to bill_transaction and bill_participats added

---
+++ database/migrations/2019_03_24_121334_add_foreign_key_bill_participants.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyBillParticipants extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_participants', function (Blueprint $table) {
+            $table->foreign('bill_participant_id_owner')->references('id')->on('bill_participants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
